43ad5f986d999b17da134ffae7e6aa9689dd0dca
sergiocormio <sergiocormio@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-03 04:12:18 +0000

    Methods to read from a file and write to a file added.

--- src/model/Utils.java
+++ src/model/Utils.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
 public class Utils {
 	public static String FIELD_SEPARATOR = "[||]"; 
+	
+	public static PrintWriter getPrintWriter(String fileName) {
+		File file = new File(fileName);
+		try {
+			return new PrintWriter(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("File: " + fileName + " cannot be created");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static BufferedReader getBufferedReader(String fileName) {
+		File file = new File(fileName);
+		if (!file.exists()) {
+			System.out.println("File: " + fileName + " not found!");
+		}
+		
+		try {
+			return new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			System.out.println("File: " + fileName + " not found!");
+		}
+		return null;
+	}
 }
