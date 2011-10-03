dfe4a57794564876df89509ad12dc0b39a6717ba
sergiocormio <sergiocormio@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-03 04:12:18 +0000

    Methods to read from a file and write to a file added.

--- trunk/src/model/GrandeTService.java
+++ trunk/src/model/GrandeTService.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class GrandeTService implements IGrandeTService{
 
 	@Override
@@ -11,6 +15,7 @@ public class GrandeTService implements IGrandeTService{
 	@Override
 	public PlayersDataBase loadPlayersDataBase(String dataBaseFileName) {
 		// TODO Auto-generated method stub
+		loadPlayerDataBaseFile(dataBaseFileName);
 		return null;
 	}
 
@@ -18,5 +23,27 @@ public class GrandeTService implements IGrandeTService{
 	public ResultantTeam retrieveSavedTeam(String resultFileName) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	//TODO: Look this method and re-implement it.
+	private static void loadPlayerDataBaseFile(String fileName) {
+
+		BufferedReader document = Utils.getBufferedReader(fileName);
+		if (document == null) {
+			return;
+		}
+
+		String line = "";
+		try {
+			line = document.readLine();
+			while (line != null) {
+				line = line.trim();
+				//TODO: doSomething(line) 
+				line = document.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
