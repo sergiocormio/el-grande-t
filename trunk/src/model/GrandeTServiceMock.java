df6a4c2ab2c54f2b84887878b218cb719e8c5c20
sergiocormio <sergiocormio@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-10 22:33:34 +0000

    ResultsPanel shows mock info now.

--- trunk/src/model/GrandeTServiceMock.java
+++ trunk/src/model/GrandeTServiceMock.java
@@ -30,8 +30,23 @@ public class GrandeTServiceMock implements IGrandeTService {
 
 	@Override
 	public ResultantTeam retrieveSavedTeam(String resultFileName) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultantTeam result = new ResultantTeam();
+		for (Player p : getTeamMock()){
+			result.addPlayer(p);
+		}
+		StatisticalInformation si = new StatisticalInformation();
+		si.setFinalCost(4000000L);
+		si.setTime(12);
+		si.setTotalProfit(900);
+		UserInputData usd = new UserInputData();
+		usd.setBudget(6000000L);
+		usd.setFormation("4-3-3");
+		usd.setNumberOfPlayers(11);
+		usd.setSkillToMax("Ataque");
+		si.setUserInputData(usd);
+		
+		result.setStatisticalInformation(si);
+		return result;
 	}
 
 	@Override
@@ -44,6 +59,21 @@ public class GrandeTServiceMock implements IGrandeTService {
 		return resultList;
 	}
 	
-	
+	private static List<Player> getTeamMock(){
+		List<Player> team = new ArrayList<Player>();
+		Player p = new Player("EL ARQUERO LOCO",Player.Position.ARQ,10000L);
+		team.add(p);
+		
+		p = new Player("EL Defensa LOCO",Player.Position.DEF,10000L);
+		team.add(p);
+		
+		p = new Player("EL Volante LOCO",Player.Position.VOL,10000L);
+		team.add(p);
+		
+		p = new Player("EL Delantero LOCO",Player.Position.DEL,10000L);
+		team.add(p);
+		
+		return team;
+	}
 
 }
