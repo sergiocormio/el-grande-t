00d547bd5505461908fec63fdcb83bdd1965583b
federico.deseta@gmail.com <federico.deseta@gmail.com@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-23 22:08:10 +0000

    Se agrega el save para PlayersDataBase

--- trunk/src/model/dto/PlayersDataBase.java
+++ trunk/src/model/dto/PlayersDataBase.java
@@ -1,13 +1,13 @@
 package model.dto;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import model.Fileable;
 import model.Serializable;
+import model.Utils;
 
 public class PlayersDataBase implements Fileable, Serializable {
 
@@ -56,14 +56,61 @@ public class PlayersDataBase implements Fileable, Serializable {
 	
 	@Override
 	public void saveToFile(String fileName) {
-		// TODO Auto-generated method stub
+		
+		PrintWriter writer = Utils.getPrintWriter(fileName);
+		
+		String headerLine = createHeaderLine();
+		writer.println(headerLine);
+		
+		for (Player player : this.players) {
+			
+			String playerLine = createPlayerLine(player, this.getSkillList());
+			writer.println(playerLine);
+		}
+		
+		writer.close();
 		
 	}
-
+	
+	private String createHeaderLine() {
+		String headersLine = "";
+		
+		for (String header : headers) {
+			
+			headersLine += header;
+			headersLine += Utils.FIELD_SEPARATOR;
+		}
+		
+		return headersLine;
+	}
+	
+	private String createPlayerLine(Player player, List<String> skills) {
+		String playerLine = "";
+		
+		playerLine += player.getName();
+		playerLine += Utils.FIELD_SEPARATOR;
+		playerLine += player.getPosition().name();
+		playerLine += Utils.FIELD_SEPARATOR;
+		playerLine += player.getPrice();
+		playerLine += Utils.FIELD_SEPARATOR;
+		
+		for (String strSkill : skills) {
+			Skill skill = player.getSkill(strSkill);
+			String skillValue = "0";
+			if(skill != null)
+				skillValue = String.valueOf(skill.getValue());
+			
+			playerLine += skillValue;
+			playerLine += Utils.FIELD_SEPARATOR;
+			
+		}
+		
+		return playerLine;
+	}
+	
 	@Override
 	public void saveToFile() {
-		// TODO Auto-generated method stub
-		
+		saveToFile(this.file.getAbsolutePath());		
 	}
 
 	@Override
