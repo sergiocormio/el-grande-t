09c9734dc5ba54f32dddbfbf7d7e85af0c38c754
sergiocormio <sergiocormio@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-30 23:47:58 +0000

    Utils without warnings.

--- src/model/Utils.java
+++ src/model/Utils.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
