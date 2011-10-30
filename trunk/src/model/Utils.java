0e1336cc77abe8fe28f18d5469a492fc6aefecf3
sergiocormio <sergiocormio@35506aed-c13d-a6d4-ec2a-58368caa349c>
2011-10-30 23:47:34 +0000

    Accents Issue fixed.

--- trunk/src/model/Utils.java
+++ trunk/src/model/Utils.java
@@ -2,9 +2,12 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class Utils {
 	
@@ -13,7 +16,7 @@ public class Utils {
 	
 	public static final String EXP_FILE_BAD_FORMED_MANDATORY = "Archivo mal formado - Las primeras 3 columnas del archivo deben ser:\n\nNOMBRE\nPOSICION\nPRECIO";
 	public static final String EXP_FILE_BAD_FORMED_STAT = "Archivo mal formado - Problema en datos estadisticos";
-	public static final String EXP_INVALID_MODEL = "Modelo inválido";
+	public static final String EXP_INVALID_MODEL = "Modelo invï¿½lido";
 	public static final String EXP_PLAYER_ALREADY_EXISTS = "El jugador ya existe";
 	public static final String EXP_SKILL_ALREADY_EXISTS = "La habilidad ya existe";
 	public static final String EXP_OLD_SKILL_NOT_EXISTS = "La habilidad a modificar no existe";;
@@ -22,9 +25,12 @@ public class Utils {
 	public static PrintWriter getPrintWriter(String fileName) {
 		File file = new File(fileName);
 		try {
-			return new PrintWriter(file);
+			return new PrintWriter(file,"ISO-8859-1");
 		} catch (FileNotFoundException e) {
 			System.out.println("File: " + fileName + " cannot be created");
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -37,10 +43,14 @@ public class Utils {
 		}
 		
 		try {
-			return new BufferedReader(new FileReader(file));
+			return new BufferedReader(new InputStreamReader (new FileInputStream(file),"ISO-8859-1"));
 		} catch (FileNotFoundException e) {
 			System.out.println("File: " + fileName + " not found!");
 			throw e;
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
 		}
 		
 	}
